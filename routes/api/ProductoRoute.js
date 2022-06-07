49a8a47c2eb7ccb2a38accd42d52998b479ce57a
David Vazquez <david.437018@gmail.com>
2022-06-06 22:11:40 -0500

    main

--- routes/api/ProductoRoute.js
+++ routes/api/ProductoRoute.js
@@ -18,7 +18,7 @@ router.get('/:id', async (req, res) =>{
     const idproducto = req.params.id;
     console.log(usuarioId)
     const productos = await sequelize.query(
-        'SELECT id_producto, nombreproducto, productos.descripcion, categorias.descripcion, precio, modelo, productos.id_usuario,nombrecategoria, productos.id_categoria, productos.createdAt, productos.updatedAt FROM productos, categorias, usuarios WHERE categorias.id_categoria = productos.id_categoria and productos.id_usuario = usuarios.id_usuario and productos.id_usuario ='+usuarioId + 'and id_producto =' + idproducto, {
+        'SELECT id_producto, nombreproducto, productos.descripcion, categorias.descripcion, precio, modelo, productos.id_usuario,nombrecategoria, productos.id_categoria, productos.createdAt, productos.updatedAt FROM productos, categorias, usuarios WHERE categorias.id_categoria = productos.id_categoria and productos.id_usuario = usuarios.id_usuario and productos.id_usuario = '+ usuarioId + ' and id_producto = ' + idproducto, {
         model: Producto,
         mapToModel: true // pass true here if you have any mapped fields
       }); 
